cb9d5db6dcc21c0cac274b5ece3ab1a26fa5f105
Kartikey2011yadav <nykay10968@gmail.com>
2024-03-07 22:02:40 +0530

    java

---
+++ src/main/webapp/success.java
@@ -0,0 +1,30 @@
+package main.webapp;
+
+import com.sun.net.httpserver.HttpServer;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/success")
+public class success extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("Name");
+        String email = req.getParameter("Email");
+        String tech = req.getParameter("tech");
+
+        System.out.println(name+" "+email+" "+tech);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException
+    {
+
+        doGet(request, response);
+    }
+}
