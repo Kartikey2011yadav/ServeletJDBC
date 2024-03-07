21a3d21e30c0ca69d22261da4bdeb92f144b9f3a
Kartikey2011yadav <nykay10968@gmail.com>
2024-03-07 20:55:42 +0530

    update front

---
+++ src/main/temp/custom.js
@@ -0,0 +1,25 @@
+(function(){
+	if(typeof _bsa !== 'undefined' && _bsa) {
+  		// format, zoneKey, segment:value, options
+  		_bsa.init('flexbar', 'CKYI627U', 'placement:w3layoutscom');
+  	}
+})();
+(function(){
+	if(typeof _bsa !== 'undefined' && _bsa) {
+		// format, zoneKey, segment:value, options
+		_bsa.init('fancybar', 'CKYDL2JN', 'placement:demo');
+	}
+	})();
+	(function(){
+		if(typeof _bsa !== 'undefined' && _bsa) {
+			  // format, zoneKey, segment:value, options
+			  _bsa.init('stickybox', 'CKYI653J', 'placement:w3layoutscom');
+		  }
+	})();
+	window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-98H8KRKT85');
+  addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); }
