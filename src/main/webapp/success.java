03bfd19a816d7f4c84ce73aceaf1d0e031c1cca6
Kartikey2011yadav <nykay10968@gmail.com>
2024-03-08 12:24:05 +0530

    File upload

--- src/main/webapp/success.java
+++ src/main/webapp/success.java
@@ -7,8 +7,11 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 
 @WebServlet("/success")
 @MultipartConfig(
@@ -22,6 +25,18 @@ public class success extends HttpServlet {
         String name = req.getParameter("Name");
         String email = req.getParameter("Email");
         String tech = req.getParameter("tech");
+
+        Part filePart = req.getPart("attachment");
+        String fileName = filePart.getSubmittedFileName();
+
+        for (Part part : req.getParts()) {
+            part.write("/run/media/darth-kartikey/Drive/ServeletJDBC/src/main/temp/" + fileName);
+        }
+        PrintWriter out =  resp.getWriter();
+        out.println("<script type=\"text/javascript\">");
+        out.println("alert('User or password incorrect');");
+        out.println("</script>");
+        resp.sendRedirect("index.html");
 
         System.out.println(name+" "+email+" "+tech);
     }
