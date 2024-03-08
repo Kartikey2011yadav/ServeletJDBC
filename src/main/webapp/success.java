708c7678b5d3dc598893b453d1ec91a70cf5b56a
Kartikey2011yadav <nykay10968@gmail.com>
2024-03-08 11:36:19 +0530

    File upload

--- src/main/webapp/success.java
+++ src/main/webapp/success.java
@@ -2,6 +2,7 @@ package main.webapp;
 
 import com.sun.net.httpserver.HttpServer;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,6 +11,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/success")
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 10,  // 10 KB
+        maxFileSize = 1024 * 300,       // 300 KB
+        maxRequestSize = 1024 * 1024    // 1 MB
+)
 public class success extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
